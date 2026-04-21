d734ee835945b184a21857698dc9b33916d1c06d
emma klatscht <51996726+tr1ppy9in3@users.noreply.github.com>
2026-04-21 22:15:53 +0700

    feat: realized reverese-proxy service

---
+++ services/kira-bff/Kira.Bfff/Extensions/BootstrapExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Kira.Bfff.Middlewares;
+
+namespace Kira.Bfff.Extensions;
+
+public static class BootstrapExtensions
+{
+    public static IServiceCollection AddBffInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll", policy =>
+            {
+                policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+            });
+        });
+
+        services
+            .AddReverseProxy()
+            .LoadFromConfig(configuration.GetSection("ReverseProxy"));
+
+        services.AddEndpointsApiExplorer();
+        return services;
+    }
+
+    public static IApplicationBuilder UseBffPipeline(this WebApplication app)
+    {
+        app.UseCors("AllowAll");
+
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/security/swagger.json", "Kira Security API");
+            c.RoutePrefix = "swagger";
+        });
+
+        app.UseMiddleware<SwaggerTransformMiddleware>();
+        app.MapReverseProxy();
+
+        var serviceName = app.Configuration["ServiceName"] ?? "Kira.BFF";
+        app.MapGet("/", () => serviceName);
+
+        return app;
+    }
+}
---
+++ services/kira-bff/Kira.Bfff/Middlewares/SwaggerTransformMiddleware.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.Json.Nodes;
+
+namespace Kira.Bfff.Middlewares;
+
+public sealed class SwaggerTransformMiddleware(RequestDelegate next)
+{
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var path = context.Request.Path.Value;
+        
+        if (path != null && path.StartsWith("/swagger/") && path.EndsWith("swagger.json"))
+        {
+            var originalBodyStream = context.Response.Body;
+            using var memoryStream = new MemoryStream();
+            context.Response.Body = memoryStream;
+
+            await next(context);
+
+            if (context.Response.StatusCode == StatusCodes.Status200OK)
+            {
+                await TransformSwaggerJson(context, memoryStream, originalBodyStream, path);
+            }
+            else
+            {
+                memoryStream.Position = 0;
+                await memoryStream.CopyToAsync(originalBodyStream);
+            }
+        }
+        else
+        {
+            await next(context);
+        }
+    }
+
+    private static async Task TransformSwaggerJson(HttpContext context, MemoryStream memoryStream, Stream originalStream, string path)
+    {
+        memoryStream.Position = 0;
+        using var reader = new StreamReader(memoryStream);
+        var responseBody = await reader.ReadToEndAsync();
+
+        try
+        {
+            var jsonNode = JsonNode.Parse(responseBody);
+            var pathsObj = jsonNode?["paths"]?.AsObject();
+            
+            if (pathsObj != null)
+            {
+                var newPaths = new JsonObject();
+                var segment = path.Split('/')[2];
+
+                foreach (var property in pathsObj.ToArray())
+                {
+                    var newPathKey = property.Key.Replace("/api/v1/", $"/api/v1/{segment}/");
+                    newPaths.Add(newPathKey, property.Value?.DeepClone());
+                }
+
+                jsonNode!["paths"] = newPaths;
+                var modifiedJson = jsonNode.ToJsonString();
+                var modifiedBytes = System.Text.Encoding.UTF8.GetBytes(modifiedJson);
+
+                context.Response.ContentLength = modifiedBytes.Length;
+                await originalStream.WriteAsync(modifiedBytes);
+            }
+        }
+        catch
+        {
+            memoryStream.Position = 0;
+            await memoryStream.CopyToAsync(originalStream);
+        }
+    }
+}
